db6af50a1a7f8c6f32d3fa1029cd911e0527dee5
mungsil <thddms2009@naver.com>
2023-11-23 15:26:30 +0900

    openSW assginment -2

---
+++ telegramBot.py
@@ -0,0 +1,32 @@
+import telegram
+import asyncio
+import schedule
+import time
+import pytz
+import datetime
+from openai import OpenAI
+
+token = "6928016911:AAEfmD-uwp2jYpTxdN35vlI18BvqfpJ_x3M"
+public_chat_name = "@kopensw"
+bot = telegram.Bot(token=token)
+
+async def send_message():
+    now = datetime.datetime.now(pytz.timezone('Asia/Seoul'))
+    if now.hour >= 23 or now.hour <= 6:
+        return
+
+    ct = "current time = " + str(now)
+    
+    await bot.sendMessage(chat_id=public_chat_name, text=ct)
+
+def job():
+    asyncio.create_task(send_message())
+
+async def main():
+    schedule.every(1).minutes.do(job)
+    while True:
+        schedule.run_pending()
+        await asyncio.sleep(1)
+
+if __name__ == "__main__":
+    asyncio.run(main())
